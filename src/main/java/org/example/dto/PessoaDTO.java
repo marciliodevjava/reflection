0be6e8797a23969644d1d0950f6170ab2dadae86
marcsilva <marciliodevjava@gmail.com>
2024-08-07 20:00:43 -0300

    Update PessoaDto

--- src/main/java/org/example/dto/PessoaDTO.java
+++ src/main/java/org/example/dto/PessoaDTO.java
@@ -4,6 +4,11 @@ public class PessoaDTO {
     private String nome;
     private String cpf;
 
+    public PessoaDTO(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
     public String getNome() {
         return nome;
     }
