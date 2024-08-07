42a496b9c6f77e1e81eccb3971a9ab4a58e619d0
marcsilva <marciliodevjava@gmail.com>
2024-08-07 20:00:36 -0300

    Update PessoaService

--- src/main/java/org/example/service/PessoaService.java
+++ src/main/java/org/example/service/PessoaService.java
@@ -1,10 +1,14 @@
 package org.example.service;
 
+import org.example.domain.Pessoa;
 import org.example.dto.PessoaDTO;
+import org.example.repository.PessoaRepository;
 
 public class PessoaService {
 
-    public PessoaDTO list(){
-
+    public PessoaDTO list() {
+        Pessoa pessoa = new PessoaRepository().list();
+        PessoaDTO pessoaDto = new PessoaDTO(pessoa.getNome(), pessoa.getCpf());
+        return pessoaDto;
     }
 }
