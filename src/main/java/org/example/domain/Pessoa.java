aa9c78aa401f6fa11678eac00c2d7bf61d6cce60
marcsilva <marciliodevjava@gmail.com>
2024-08-07 19:55:47 -0300

    Create Pessoa

---
+++ src/main/java/org/example/domain/Pessoa.java
@@ -0,0 +1,38 @@
+package org.example.domain;
+
+public class Pessoa {
+    private Long id;
+    private String nome;
+    private String cpf;
+
+    public Pessoa(Integer id, String nome, String cpf) {
+        this.id = Long.valueOf(id);
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+}
