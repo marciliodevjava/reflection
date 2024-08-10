aec5ffc14cdbffae456cca4db74eda0439dcc97f
marcsilva <marciliodevjava@gmail.com>
2024-08-09 21:17:45 -0300

    Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,12 @@ package org.example;
 import org.example.service.PessoaService;
 
 public class Main {
+    private static final String INICIO = "INICIANDO SERVICO.";
+    private static final String FINALIZANDO = "FINALIZANDO SERVIÇO.";
+
     public static void main(String[] args) {
+        System.out.println(INICIO);
         new PessoaService().list();
+        System.out.println(FINALIZANDO);
     }
 }
