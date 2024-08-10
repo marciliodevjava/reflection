5f5c70984b6618d05b5be25e9c3f3eb1b5506a5b
marcsilva <marciliodevjava@gmail.com>
2024-08-10 19:20:32 -0300

    Update PessoaService

--- src/main/java/org/example/service/PessoaService.java
+++ src/main/java/org/example/service/PessoaService.java
@@ -5,10 +5,14 @@ import org.example.dto.PessoaDTO;
 import org.example.repository.PessoaRepository;
 
 public class PessoaService {
+    private final String MESSAGE_CREATE = "CRIANDO O OBJETO PESSOA";
+    private final String MESSAGE_FINAL = "RETORNANDO O OBJETO PESSOA";
 
     public PessoaDTO list() {
+        System.out.println(MESSAGE_CREATE);
         Pessoa pessoa = new PessoaRepository().list();
         PessoaDTO pessoaDto = new PessoaDTO(pessoa.getNome(), pessoa.getCpf());
+        System.out.println(MESSAGE_FINAL);
         return pessoaDto;
     }
 }
