3b9793bb94aca4ae4f8f8cae3aec0d7970b676c8
marcsilva <marciliodevjava@gmail.com>
2024-08-15 22:35:48 -0300

    Update a classe Transformator

--- src/main/java/org/example/refl/Transformator.java
+++ src/main/java/org/example/refl/Transformator.java
@@ -2,8 +2,9 @@ package org.example.refl;
 
 public class Transformator {
 
-    public <I, O> transform(I input){
+    public <I, O> transform(I input) throws ClassNotFoundException {
         Class<?> source = input.getClass();
+        Class<?> target = source.forName(source.getClass() + "DTO");
 
     }
 }
