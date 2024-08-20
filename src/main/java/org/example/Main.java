1b4da967ae72cbe373dacc9982453a831a2729b9
marcsilva <marciliodevjava@gmail.com>
2024-08-20 20:03:37 -0300

    Ajustando o metodo main.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,5 @@
 package org.example;
 
-import org.example.dto.PessoaDTO;
-import org.example.refl.Transformator;
 import org.example.service.PessoaService;
 
 import java.lang.reflect.InvocationTargetException;
