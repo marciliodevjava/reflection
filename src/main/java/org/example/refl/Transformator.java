3c6d00e37fb70c37095707840aaa2e6b7d8b2c6f
marcsilva <marciliodevjava@gmail.com>
2024-08-19 21:12:46 -0300

    Criando metodo que utiliza relection.

--- src/main/java/org/example/refl/Transformator.java
+++ src/main/java/org/example/refl/Transformator.java
@@ -1,10 +1,14 @@
 package org.example.refl;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class Transformator {
 
-    public <I, O> transform(I input) throws ClassNotFoundException {
+    public <I, O> O transform(I input) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         Class<?> source = input.getClass();
         Class<?> target = source.forName(source.getClass() + "DTO");
 
+        O targetClass = (O) target.getDeclaredConstructor(). newInstance();
+        return targetClass;
     }
 }
