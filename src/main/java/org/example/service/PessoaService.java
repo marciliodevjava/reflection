15b82e6a71ffcec0db634d5a44def267bf6cfa60
marcsilva <marciliodevjava@gmail.com>
2024-08-20 20:37:59 -0300

    Ajustando o metodo list

--- src/main/java/org/example/service/PessoaService.java
+++ src/main/java/org/example/service/PessoaService.java
@@ -2,17 +2,20 @@ package org.example.service;
 
 import org.example.domain.Pessoa;
 import org.example.dto.PessoaDTO;
+import org.example.refl.Transformator;
 import org.example.repository.PessoaRepository;
+
+import java.lang.reflect.InvocationTargetException;
 
 public class PessoaService {
     private final String MESSAGE_CREATE = "CRIANDO O OBJETO PESSOA";
     private final String MESSAGE_FINAL = "RETORNANDO O OBJETO PESSOA";
 
-    public PessoaDTO list() {
+    public PessoaDTO list() throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
         System.out.println(MESSAGE_CREATE);
         Pessoa pessoa = new PessoaRepository().list();
-        PessoaDTO pessoaDto = new PessoaDTO(pessoa.getNome(), pessoa.getCpf());
+        PessoaDTO pessoaDTO = new Transformator().transform(pessoa);
         System.out.println(MESSAGE_FINAL);
-        return pessoaDto;
+        return pessoaDTO;
     }
 }
