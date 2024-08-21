6118b6c23ab842f2ff0253d8582b1044c87d3477
marcsilva <marciliodevjava@gmail.com>
2024-08-21 17:25:39 -0300

    Ajuste na classe TransformatorTest

--- src/test/java/org/example/TransformatorTest.java
+++ src/test/java/org/example/TransformatorTest.java
@@ -1,11 +1,16 @@
 package org.example;
 
+import org.example.domain.Pessoa;
 import org.example.refl.Transformator;
+
+import java.lang.reflect.InvocationTargetException;
 
 public class TransformatorTest {
 
-    public void shouldTransform(){
+    Pessoa pessoa = new Pessoa(1, "Joaquim", "02545678981");
+
+    public void shouldTransform() throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
         Transformator transformator = new Transformator();
-        transformator.transform()
+        transformator.transform(pessoa);
     }
 }
