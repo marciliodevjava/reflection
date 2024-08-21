ba62522e2339164a84994be5ee33bab22491a073
marcsilva <marciliodevjava@gmail.com>
2024-08-21 10:27:34 -0300

    Atualizando classe TransformatorTest

--- src/test/java/org/example/TransformatorTest.java
+++ src/test/java/org/example/TransformatorTest.java
@@ -1,4 +1,11 @@
 package org.example;
 
+import org.example.refl.Transformator;
+
 public class TransformatorTest {
+
+    public void shouldTransform(){
+        Transformator transformator = new Transformator();
+        transformator.transform()
+    }
 }
