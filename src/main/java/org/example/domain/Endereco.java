53c0a91fc1cae9764bbbb5c744c6668eee8ecbe4
marcsilva <marciliodevjava@gmail.com>
2024-08-23 20:40:51 -0300

    Criação a class Endereco.

---
+++ src/main/java/org/example/domain/Endereco.java
@@ -0,0 +1,30 @@
+package org.example.domain;
+
+public class Endereco {
+    private String rua;
+    private Integer numero;
+
+    public Endereco(){
+    }
+
+    public Endereco(String rua,Integer numero){
+        this.rua = rua;
+        this.numero = numero;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+}
