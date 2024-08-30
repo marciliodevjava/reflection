bedf43dfa32913ede5ae49b41f3e2c4e1c640ecc
marcsilva <marciliodevjava@gmail.com>
2024-08-29 21:00:50 -0300

    Finalizando a classe para transofrma uma classe em string.

--- src/main/java/org/example/refl/ObjectToJson.java
+++ src/main/java/org/example/refl/ObjectToJson.java
@@ -1,19 +1,41 @@
 package org.example.refl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ObjectToJson {
 
     public String transform(Object object) {
+        String result = null;
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
 
         Map<String, Object> mapper = new HashMap<String, Object>();
+        Class<?> classToBeTransformed = object.getClass();
 
-        return null;
+        Arrays.stream(classToBeTransformed.getDeclaredFields()).toList().forEach(
+                field -> {
+                    field.setAccessible(true);
+                    String key = field.getName();
+                    Object value = null;
+                    try {
+                        value = field.get(object);
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException(e);
+                    }
+                    mapper.put(key, value);
+                }
+        );
+        try {
+            result = objectMapper.writeValueAsString(mapper);
+        } catch (JsonProcessingException exception) {
+            exception.getMessage();
+        }
+        return result;
     }
 }
