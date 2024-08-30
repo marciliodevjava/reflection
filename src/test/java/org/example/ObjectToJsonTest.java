0f36a1b443bfc89cd42dbef2e34a6c44ccea9516
marcsilva <marciliodevjava@gmail.com>
2024-08-29 21:06:40 -0300

    Criando uma classe para testar a classe de transformação json.

--- src/test/java/org/example/ObjectToJsonTest.java
+++ src/test/java/org/example/ObjectToJsonTest.java
@@ -1,8 +1,14 @@
 package org.example;
 
+import org.example.domain.Pessoa;
+import org.example.refl.ObjectToJson;
+
 public class ObjectToJsonTest {
 
-    public static void main(String... x){
+    static Pessoa pessoa = new Pessoa(1, "Gustavo", "02596345681");
 
+    public static void main(String... x) {
+        ObjectToJson objectToJson = new ObjectToJson();
+        System.out.println(objectToJson.transform(pessoa));
     }
 }
